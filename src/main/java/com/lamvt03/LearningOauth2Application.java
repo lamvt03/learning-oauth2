c1bf53ecec1d9cbb7bea69ae3f2c926604846bcc
Vo Truong Lam <votruonglam2109@gmail.com>
2024-04-11 19:30:17 +0700

    init

---
+++ src/main/java/com/lamvt03/LearningOauth2Application.java
@@ -0,0 +1,13 @@
+package com.lamvt03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningOauth2Application.class, args);
+	}
+
+}
